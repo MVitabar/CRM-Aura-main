80404cccb64b786bbd44ae6f299fed2c17fcbdaf
Rocco Aguilera <roccojuegos@gmail.com>
2024-10-05 23:14:03 -0500

    Clean project: Proyecto totalmente limpio.

--- src/views/eventManager/EventManagerContainer.tsx
+++
@@ -1,6 +0,0 @@
-import capa2 from "../../assets/Capa_2.svg";
-
-function EventManagerContainer() {
-  return <img src={capa2} alt="" />;
-}
-export default EventManagerContainer;
--- src/views/statistics/StatisticsContainer.tsx
+++
@@ -1,6 +0,0 @@
-import capa1 from "../../assets/Capa_1.svg";
-
-function StatisticsContainer() {
-  return <img src={capa1} alt="" />;
-}
-export default StatisticsContainer;
